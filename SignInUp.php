de489913ba83c24b3aa2902ae3f24844aca11819
havoc4869 <havoc4869@gmail.com>
2017-12-07 02:51:49 +0700

    fixed include

---
+++ SignInUp.php
@@ -0,0 +1,45 @@
+<div>
+    <ul class="top">
+        <li><a href="#">
+            <img src="img/logo/Logo.png" style="height: 45px; margin-left: 10px; margin-top: -12px; ">
+        </a></li>
+
+        <li style="float: right;"><a href="#" onclick="document.getElementById('id01').style.display='block'" style="width:auto;">Sign In</a></li>
+            <div id="id01" class="modal">
+                <form class="modal-content animate" action="/action_page.php">
+                    <div class="imgcontainer">
+                        <span onclick="document.getElementById('id01').style.display='none'" class="close" title="Close Modal">&times;</span>
+                        <img src="" alt="Avatar" class="avatar">
+                    </div>
+
+                    <div class="container">
+                        <label><b>Username</b></label>
+                        <input type="text" placeholder="Enter Username" name="uname" required>
+                        <label><b>Password</b></label>
+                        <input type="password" placeholder="Enter Password" name="psw" required>
+                        <button type="submit">Login</button>
+                        <input type="checkbox" checked="checked"> Remember me
+                    </div>
+
+                    <div class="container" style="background-color:#f1f1f1">
+                        <button type="button" onclick="document.getElementById('id01').style.display='none'" class="cancelbtn">Cancel</button>
+                        <span class="psw">Forgot <a href="#">password?</a></span>
+                    </div>
+                </form>
+            </div>
+
+            <script>
+            // Get the modal
+            var modal = document.getElementById('id01');
+
+            // When the user clicks anywhere outside of the modal, close it
+            window.onclick = function(event) {
+                if (event.target == modal) {
+                    modal.style.display = "none";
+                }
+            }
+            </script>
+
+        <li style="float: right;"><a href="#">Sign Up</a></li>        
+    </ul>
+    </div>
---
+++ sliderku.php
@@ -0,0 +1,11 @@
+<div id="slider"  class="container_Utama" style="height: 350px;">
+	<figure>
+	<img src="">
+	<img src="img/Slider/slider1.jpg" alt>
+	<img src="img/Slider/slider2.jpg" alt>
+	<img src="img/Slider/slider3.jpg" alt>
+	<img src="img/Slider/slider4.jpg" alt>
+	<img src="img/Slider/slider5.jpg" alt>
+	</base>
+	</figure>
+	</div>
