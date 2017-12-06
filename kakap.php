a2f48fd847fbbd1a801a2f710b4d2ca171f96a0e
havoc4869 <havoc4869@gmail.com>
2017-12-06 22:40:07 +0700

    page

---
+++ kakap.php
@@ -0,0 +1,172 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>PT Arona Wijaya Sakti</title>
+	<link rel="stylesheet" type="text/css" href="css.css">
+	<link href="css/bootstrap.css" rel="stylesheet">
+	<link href="css/bootshape.css" rel="stylesheet">
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+</head>
+<body class="bgUtama">
+
+	
+	<div id="slider"  class="container_Utama" style="height: 300px; margin: 5% 5% 0;">
+	<figure>
+	<img src="">
+	<img src="img/Slider/slider1.jpg" alt>
+	<img src="img/Slider/slider2.jpg" alt>
+	<img src="img/Slider/slider3.jpg" alt>
+	<img src="img/Slider/slider4.jpg" alt>
+	<img src="img/Slider/slider5.jpg" alt>
+	</base>
+	</figure>
+	</div>
+
+
+	<div>
+	<ul class="topnav">
+		<li><a href="index.php">Beranda</a></li>
+ 		<li class="menu-mudun"><a href="#">Produk
+		<i class="fa fa-caret-down"></i></a>
+			<ul class="mudun">
+				<li><a href="udang.php">Udang</a></li>
+				<li><a href="nila.php">Ikan Nila</a></li>
+				<li><a href="kakap.php">Ikan Kakap</a></li>
+				<li><a href="bandeng.php">Ikan Bandeng</a></li>
+			</ul>
+		</li> 
+  		<li><a href="#">Order</a></li>
+  		<li><a href="tentang.php">Tentang Kami</a></li>
+  		<li><a href="#">Hubungi Kami</a></li>
+		<li class="icon">
+    <a href="javascript:void(0);" onclick="myFunction()">☰</a>
+  </li></a>
+	</ul>
+	</div>
+
+	<div class="container_Utama">
+		<div class="col-sm-9" style="background-color: #CEEAFF;">
+<p><img src="img/kakap.jpg" height="60%" width="60%" style="float:center;margin:8px 4px 4px 8px;"></p>
+
+<h2>Klasifikasi dan Morfologi Udang Vanamei</h2>
+<ol style="margin:4px 0 0 15px">
+	<li>Klasifikasi</li>
+	<table border="10px">
+	<tr><td>
+		<p>Kingdom : Animalia</p>
+		<p>Filum : Chordata</p>
+		<p>Kelas : Actinopterygii</p>
+		<p>Ordo : Perciformes</p>
+		</td><td></td><td></td><td>
+		<p>Famili : Lutjanidae</p>
+		<p>Genus : Lutjanus</p>
+		<p>Spesies : Lutjanus campechanus</p></td>
+	</tr>
+		</table>
+	<li>Morfologi</li>
+	<p> &nbsp &nbsp&nbsp Ikan kakap memiliki dua jenis, dibedakan berdasarkan warna yakni :</p>
+		<ul style="margin:1px 0 0 60px;">
+			<li>Kakap Merah</li>
+				<p><img src="img/kakap-merah.jpg" height="50%" width="50%" style="float:left;margin:8px 4px 4px 8px;">&nbsp &nbsp&nbsp Ikan kakap merah ini memiliki badan yang lebar dan juga memanjang. Kepala dari ikan ini juga memiliki bentuk yang gepeng serta agak cembung dan sirip punggung memiliki beberapa ruas. Pada bagian bawah penutup insangnya, terdapat semacam gerigi-gerigi yang berkontur kasar. Ikan ini mempunyai gigi-gigi yang tersusun di dalam rahang dengan organ gigi taring yang berada di posisi rahang atas sebelah paling luar. Ikan kakap merah sendiri termasuk kedalam jenis binatang karnivora.</p>
+				<p> &nbsp &nbsp&nbsp Ikan ini juga memangsa ikan-ikan kecil serta mahluk invertebrata yang hidup di dasar permukaan laut. Ukuran tubuh dari kakap merah dapat mencapai panjang maksimal hingga sepanjang 45-50 cm. Ikan ini sendiri dinamakan ikan kakap merah karena ikan kakap yang satu ini mempunyai sisik berwarna kemerah-merahan. Sedangkan untuk bagian tengah tubuhnya agak berkelir putih dan bercorak kemerahan. Lalu pada bagian punggung di atas garis rusuknya, juga terdapat corak garis kuning yang kemerahan.</p>
+				<p> &nbsp &nbsp&nbsp Tempat tinggal ikan kakap merah terletak di daerah perairan asin yang beriklim tropis dan juga subtropis. Ikan ini umumnya hidup di lingkungan perairan yang berkarang sampai ke daerah pasang surut di muara. Beberapa spesies kakap bahkan lebih menyukai berada di dalam areal perairan tawar.Dalam hidupnya, ikan kakap merah ini selalu membentuk gerombolan-gerombolan yang cukup besar. Ikan dewasa dari jenis ikan ini biasanya tinggal di dasar perairan yang dalamnya lebih dari 40-50 meter. Di perairan ini, lingkunganya terbilang mempunyai sedikit karang, tingkat salinitas 30-33 ppt, dan suhu air 5-32 °C.</p>
+			<li>Kakap Putih</li>
+				<p> &nbsp &nbsp&nbsp Ikan kakap putih, hampir memiliki morfoligi yang sama dengan kakap merah. Yang membedakan adalah pada warna kulit dari kakap putih adalah putih mengkilat. Perbedaan lain yang mencolok adalah bentuk mulut yang lebih moncong. Kemudian, ikan kakap putih dikenal lebih agresif daripada ikan kakap merah. Namun demikian, ikan kakap merah juga memiliki keagresifan yang besar. Terbukti ketika kita memancing ikan ini, maka ketika ikan terkena sambaran umpan, dia akan langsung membawa mata kail kedasar sungai, atau bahkan keluar keatas permukaan untuk loncat. Bahkan baik ikan kakap merah maupun kakap putih bisa saja memutuskan tali senar pancing dengan mudah menggunakan giginya yang tajam.</p>
+		</ul>
+	<p>
+</ol>
+<a href="order.php" target="_blank"><button style="margin:10px 0 20px 20px;font-weight: bold;cursor: pointer;text-align: center;-webkit-border-radius: 5px;
+    border-radius: 5px;background-color:#000066;color:white;">ORDER</button></a>
+<!-- Editnya di sini ya maaaas!!! Di antara div ini-->
+
+
+
+
+
+
+		</div>
+
+		<div class="col-sm-3" style="background-color: #e5f3ff;">
+			<br><br><br><br><br><br><br><br><br><br><br>
+	<br><br><br><br><br><br><br><br><br><br><br>
+	<br><br><br><br><br><br><br><br><br><br><br>
+	<br><br><br><br><br><br><br><br><br><br><br>
+	<br><br><br><br><br>	aaaaaaaaaaaaaa
+		</div>
+	</div>
+	<div class="container_Utama" style="background-color: black;">
+	<div id="footer">
+		<div class="kaki row-md-20">
+        <div class="row">
+            <div class="col-md-4" style="padding: 20px 40px 10px;">
+            
+                <h4 style="color: white">Informasi Kami :</h4>
+
+                <p style="color: white" style="margin-left: 20px">
+	            Bumi Dipasena adalah sebagian kecil dari desa yang menghasilkan atau memproduksi udang guna di ekspor keluar negeri pada jamannya,
+				yakni sekitar tahun 90-an, sampai sekitar tahun 2007. 
+				Namun sekarang sudah tidak begitu aktif lagi mengekspor karena berbagai masalah yang muncul
+				baik dari petaninya maupun dari perusahaanya.
+				</p>
+
+                <button type="submit" class="btn btn-primary">Selengkapnya</button>
+                <hr>
+            </div>
+            <div class="col-md-4" style="padding: 20px 40px 10px;">
+                <h4 style="color: white">Butuh Bantuan ? Telepon 7276kali.<br><br>Atau Tulis disini</h4>
+                <hr>
+                <form>
+                    <div class="row">
+                        <div class="col-md-6 col-sm-6">
+                            <div class="form-group">
+                                <input type="text" class="form-control" required="required" placeholder="Nama">
+                            </div>
+                        </div>
+                        <div class="col-md-6 col-sm-6">
+                            <div class="form-group">
+                                <input type="text" class="form-control" required="required" placeholder="E-mail">
+                            </div>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-md-12 col-sm-12">
+                            <div class="form-group">
+                                <textarea name="message" id="message" required="required" class="form-control" rows="3" placeholder="Komentar"></textarea>
+                            </div>
+                            <div class="form-group">
+                            <button type="reset" class="btn btn-primary">Batal</button>
+                                <button type="submit" class="btn btn-primary">Kirim</button>
+                            </div>
+                        </div>
+                    </div>
+                </form>
+            </div>
+            <div class="col-md-4" style="padding: 20px 40px 10px;">
+
+                <hr>
+                <p style="color: white">
+                Jalan ringroad utara<br>
+                alamat web menyusul
+				</p>
+                <hr>
+                <a href="www.facebook.com"><img src="img/logo/fb.png" class="logo"></a>
+                <a href="twitter.com"><img src="img/logo/twet.jpg" class="logo"></a>
+                <a href="youtube.com"><img src="img/logo/you.jpg" class="logo"></a>
+                <a href="https://plus.google.com"><img src="img/logo/gug.jpg" class="logo"></a>
+
+            </div>
+        </div>
+
+
+    </div>
+	</div>
+	</div>
+	
+
+<script>
+function myFunction() {
+    document.getElementsByClassName("topnav")[0].classList.toggle("responsive");
+}
+</script>
+</body>
+</html>
