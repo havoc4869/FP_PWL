ff48a932e9e269c1385f83baf6d529a33227ed8b
Sheriff26 <students.amikom.ac.id>
2017-12-06 23:06:51 +0700

    login dan gambar

--- index.php
+++ index.php
@@ -3,6 +3,7 @@
 <head>
 	<title>PT Arona Wijaya Sakti</title>
 	<link rel="stylesheet" type="text/css" href="css.css">
+    <link rel="stylesheet" type="text/css" href="logincss.css">
 	<link href="css/bootstrap.css" rel="stylesheet">
 	<link href="css/bootshape.css" rel="stylesheet">
 </head>
@@ -12,9 +13,43 @@
     <div>
     <ul class="top">
         <li><a href="#">LOGO</a></li>
-        <li style="float: right;"><a href="#">Sign In</a></li>
-        <li style="float: right;"><a href="#">Sign Up</a></li>
-        
+        <li style="float: right;"><a href="#" onclick="document.getElementById('id01').style.display='block'" style="width:auto;">Sign In</a></li>
+            <div id="id01" class="modal">
+                <form class="modal-content animate" action="/action_page.php">
+                    <div class="imgcontainer">
+                        <span onclick="document.getElementById('id01').style.display='none'" class="close" title="Close Modal">&times;</span>
+                        <img src="" alt="Avatar" class="avatar">
+                    </div>
+
+                    <div class="container">
+                        <label><b>Username</b></label>
+                        <input type="text" placeholder="Enter Username" name="uname" required>
+                        <label><b>Password</b></label>
+                        <input type="password" placeholder="Enter Password" name="psw" required>
+                        <button type="submit">Login</button>
+                        <input type="checkbox" checked="checked"> Remember me
+                    </div>
+
+                    <div class="container" style="background-color:#f1f1f1">
+                        <button type="button" onclick="document.getElementById('id01').style.display='none'" class="cancelbtn">Cancel</button>
+                        <span class="psw">Forgot <a href="#">password?</a></span>
+                    </div>
+                </form>
+            </div>
+
+            <script>
+            // Get the modal
+            var modal = document.getElementById('id01');
+
+            // When the user clicks anywhere outside of the modal, close it
+            window.onclick = function(event) {
+                if (event.target == modal) {
+                    modal.style.display = "none";
+                }
+            }
+            </script>
+
+        <li style="float: right;"><a href="#">Sign Up</a></li>        
     </ul>
     </div>
 	
