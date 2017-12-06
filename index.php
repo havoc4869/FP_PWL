e776f169c3c5e5cb6f76da04e39c40272fcbe217
havoc4869 <havoc4869@gmail.com>
2017-12-07 01:38:03 +0700

    fixed

--- index.php
+++ index.php
@@ -8,28 +8,32 @@
 	<link href="css/bootshape.css" rel="stylesheet">
 </head>
 <body class="bgUtama">
+
+
     <div>
     <ul class="top">
-        <li><a href="#">LOGO</a></li>
+        <li><a href="#">
+            <img src="img/logo/Logo.png" style="height: 45px; margin-left: 10px; margin-top: -12px; ">
+        </a></li>
         <li style="float: right;"><a href="#" onclick="document.getElementById('id01').style.display='block'" style="width:auto;">Sign In</a></li>
             <div id="id01" class="modal">
-                <form class="modal-conten animate" action="/action_page.php">
+                <form class="modal-content animate" action="/action_page.php">
                     <div class="imgcontainer">
                         <span onclick="document.getElementById('id01').style.display='none'" class="close" title="Close Modal">&times;</span>
-                        <img src="D:\kampus\Semester 5\Pemrograman Web Lanjut\FP_PWL\img\logo\logo.png" alt="Avatar" class="avatar">
+                        <img src="" alt="Avatar" class="avatar">
                     </div>
 
-                    <div class="isi">
+                    <div class="container">
                         <label><b>Username</b></label>
                         <input type="text" placeholder="Enter Username" name="uname" required>
                         <label><b>Password</b></label>
                         <input type="password" placeholder="Enter Password" name="psw" required>
-                        <button class="button1" type="submit">Login</button>
+                        <button type="submit">Login</button>
                         <input type="checkbox" checked="checked"> Remember me
                     </div>
 
-                    <div class="isi" style="background-color:#f1f1f1">
-                        <button type="button" onclick="document.getElementById('id01').style.display='none'" class="cancelbtn button1">Cancel</button>
+                    <div class="container" style="background-color:#f1f1f1">
+                        <button type="button" onclick="document.getElementById('id01').style.display='none'" class="cancelbtn">Cancel</button>
                         <span class="psw">Forgot <a href="#">password?</a></span>
                     </div>
                 </form>
