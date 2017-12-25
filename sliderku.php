4804dd647e3633b323bc2c990192ea76c565daf9
Mozaik Ramadhan <mozaikramadhan0@gmail.com>
2017-12-26 00:42:18 +0700

    ngising

--- sliderku.php
+++ sliderku.php
@@ -1,6 +1,5 @@
 <div id="slider"  class="container_Utama" style="height: 350px;">
 	<figure>
-	<img src="">
 	<img src="img/Slider/slider1.jpg" alt>
 	<img src="img/Slider/slider2.jpg" alt>
 	<img src="img/Slider/slider3.jpg" alt>
